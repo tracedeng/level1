7f81b6d7a1f1f5bf82c67107154e3c4fe151c158
tracedeng <1401733434@qq.com>
2016-03-14 16:12:15 +0800

    run.py

--- run.py
+++ run.py
@@ -2,7 +2,8 @@
 __author__ = 'tracedeng'
 
 import sys
-wrapper_path = ["./lv1/", "./coframe", "../level2/wrapper", "../level2/proto", "../level2/proto/lv2", "../level2/lv2", "../level2/proto/branch"]
+wrapper_path = ["./lv1/", "./coframe", "../level2/wrapper", "../level2/proto", "../level2/proto/lv2",
+                "../level2/proto/branch"]
 for path in wrapper_path:
     if path not in sys.path:
         print("add path %s", path)
